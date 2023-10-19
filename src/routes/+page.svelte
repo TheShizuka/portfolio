897143d821733eeac3a12bc95cc417034e207ab1
TheShizuka <77496700+TheShizuka@users.noreply.github.com>
2023-10-19 06:57:03 +0200

    wbesite test +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,58 @@
-<script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
-</script>
-
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Name or Brand</title>
+    <!-- Replace the href with your chosen font's URL or link to the 'Tilt Neon' font file if it's hosted locally -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Tilt+Neon&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>Your Name or Brand</h1>
+        <nav>
+            <ul>
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">Design</a></li>
+                <li><a href="#">Coding Projects</a></li>
+                <li><a href="#">3D Content</a></li>
+                <li><a href="#">Photography</a></li>
+                <li><a href="#">Videography</a></li>
+            </ul>
+        </nav>
+    </header>
+    <div class="banner">
+        <img src="images/banner.png" alt="Banner Description" class="banner-image">
+    </div>
+    <!-- Image Rows with "Learn More" buttons -->
+	<div class="about">
+        <img src="images/profile-picture.png" alt="Learn more about my photography skills">
+		<div class="text-about">
+		<h3>Shizuka</h3>
+        <p>Passionate and versatile digital creator with a diverse skill set spanning video/photo editing, 3D
+			modeling, 2D art, and web development. Proficient in Adobe Suite, including Photoshop,
+			Illustrator, Premiere Pro, and After Effects. Eager to contribute to innovative projects with a keen
+			interest in remote roles related to video/photo creation, web design, and 3D artistry. Committed
+			to continuous learning and growth in the digital domain. In addition to my digital creation skills,
+			I also possess coding expertise, enabling me to develop and implement technical solutions for
+			various projects.
+			</p>
+		</div>
+    </div>
+    <div class="image-row">
+        <img src="images/photography.png" alt="Learn more about my photography skills">
+        <button>Learn More</button>
+    </div>
+    <div class="image-row">
+        <img src="path_to_image2.jpg" alt="Description of Image 2">
+        <button>Learn More</button>
+    </div>
+    <div class="image-row">
+        <img src="path_to_image3.jpg" alt="Description of Image 3">
+        <button>Learn More</button>
+    </div>
+</body>
+</html>
