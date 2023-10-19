8891cc74357fb2f9907e37bca6ddd8e619f12b17
TheShizuka <77496700+TheShizuka@users.noreply.github.com>
2023-10-19 10:42:08 +0200

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -21,12 +21,12 @@
         <h1>Your Name or Brand</h1>
         <nav>
             <ul>
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#">Design</a></li>
-                <li><a href="#">Coding Projects</a></li>
-                <li><a href="#">3D Content</a></li>
-                <li><a href="#">Photography</a></li>
-                <li><a href="#">Videography</a></li>
+                <li class="active"><a href="/">Home</a></li>
+                <li><a href="/Design">Design</a></li>
+                <li><a href="/Developer">Coding Projects</a></li>
+                <li><a href="/3D ART">3D Content</a></li>
+                <li><a href="/Photography">Photography</a></li>
+                <li><a href="/Videography">Videography</a></li>
             </ul>
         </nav>
     </header>
