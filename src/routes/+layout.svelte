ea295b20e8868bf631073427d9622e7dc97de5ed
TheShizuka <77496700+TheShizuka@users.noreply.github.com>
2023-10-19 12:01:53 +0200

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,18 +10,21 @@
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Tilt+Neon&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="styles.css">
-<h1>Shizuka</h1>
+
+</header>
+<div class="header">
+        <h1>Your Name or Brand</h1>
         <nav>
             <ul>
-                <li class="active"><a href="/">Home</a></li>
-                <li><a href="/design">Design</a></li>
-                <li><a href="/Developer">Coding Projects</a></li>
-                <li><a href="/3D ART">3D Content</a></li>
-                <li><a href="/Photography">Photography</a></li>
-                <li><a href="/Videography">Videography</a></li>
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">Design</a></li>
+                <li><a href="#">Coding Projects</a></li>
+                <li><a href="#">3D Content</a></li>
+                <li><a href="#">Photography</a></li>
+                <li><a href="#">Videography</a></li>
             </ul>
         </nav>
-</header>
+    </div>
 <main>
 <slot></slot>
 </main>
