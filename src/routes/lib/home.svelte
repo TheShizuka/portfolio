905bb6ca5b8787d85fad18b6ab1c7f0a59c0b51f
TheShizuka <77496700+TheShizuka@users.noreply.github.com>
2023-10-19 10:15:21 +0200

    Create home.svelte

---
+++ src/routes/lib/home.svelte
@@ -0,0 +1,37 @@
+
+<script>
+
+</script>
+
+
+
+<div class="banner">
+        <img src="images/banner.png" alt="Banner Description" class="banner-image">
+    </div>
+    <!-- Image Rows with "Learn More" buttons -->
+	<div class="about">
+        <img src="images/profile-picture.png" alt="Learn more about my photography skills">
+		<div class="text-about">
+		<h3>Shizuka</h3>
+        <p>Passionate and versatile digital creator with a diverse skill set spanning video/photo editing, 3D
+			modeling, 2D art, and web development. Proficient in Adobe Suite, including Photoshop,
+			Illustrator, Premiere Pro, and After Effects. Eager to contribute to innovative projects with a keen
+			interest in remote roles related to video/photo creation, web design, and 3D artistry. Committed
+			to continuous learning and growth in the digital domain. In addition to my digital creation skills,
+			I also possess coding expertise, enabling me to develop and implement technical solutions for
+			various projects.
+			</p>
+		</div>
+    </div>
+    <div class="image-row">
+        <img src="images/photography.png" alt="Learn more about my photography skills">
+        <button>Learn More</button>
+    </div>
+    <div class="image-row">
+        <img src="path_to_image2.jpg" alt="Description of Image 2">
+        <button>Learn More</button>
+    </div>
+    <div class="image-row">
+        <img src="path_to_image3.jpg" alt="Description of Image 3">
+        <button>Learn More</button>
+    </div>
