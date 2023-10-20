29d60f884b67cdafec4b9db077d7456f45d9a52a
TheShizuka <77496700+TheShizuka@users.noreply.github.com>
2023-10-20 11:37:54 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,35 @@
 <script>
+  // Svelte Component Script
+  let imageCards = [
+    {
+      title: 'Title 1',
+      description: 'Description for Image 1',
+      link: '#',
+      imgSrc: '',
+      alt: 'Description of Image 1'
+    },
+    {
+      title: 'Title 2',
+      description: 'Description for Image 2',
+      link: '#',
+      imgSrc: '',
+      alt: 'Description of Image 2'
+    },
+    // ... Add more image card objects here
+  ];
 
+  function navigateTo(link) {
+    window.location.href = link;
+  }
 </script>
 
 <div class="index">
 
-<div class="banner">
-        <img src="../images/banner.png" alt="Banner Description" class="banner-image">
-    </div>
-    <!-- Image Rows with "Learn More" buttons -->
-	<div class="about">
+  <div class="banner">
+    <img src="../images/banner.png" alt="Banner Description" class="banner-image">
+  </div>
+
+  <div class="about">
         <img src="" alt="Learn more about my photography skills">
 		<div class="text-about">
 		<h3>Shizuka</h3>
@@ -22,16 +43,16 @@
 			</p>
 		</div>
     </div>
-    <div class="image-row">
-        <img src="" alt="Learn more about my photography skills">
-        <button>Learn More</button>
-    </div>
-    <div class="image-row">
-        <img src="" alt="Description of Image 2">
-        <button>Learn More</button>
-    </div>
-    <div class="image-row">
-        <img src="" alt="Description of Image 3">
-        <button>Learn More</button>
-    </div>
+
+  <div class="image-row">
+    {#each imageCards as { title, description, link, imgSrc, alt } (title)}
+      <div class="image-card" on:click={() => navigateTo(link)}>
+        <img src={imgSrc} alt={alt}>
+        <div>
+          <h4>{title}</h4>
+          <p>{description}</p>
+        </div>
+      </div>
+    {/each}
   </div>
+</div>
