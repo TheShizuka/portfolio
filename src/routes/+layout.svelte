65848bb0ec8f84d4720947081c94a0ad4f3246fd
TheShizuka <77496700+TheShizuka@users.noreply.github.com>
2023-10-20 13:58:59 +0200

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@
 <header>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Your Name or Brand</title>
+	<title>Shizuka's Portfolio</title>
 	<!-- Replace the href with your chosen font's URL or link to the 'Tilt Neon' font file if it's hosted locally -->
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
