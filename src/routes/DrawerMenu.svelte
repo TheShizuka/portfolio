c09d36aea9cbff08e7b22b3c25131a319207f378
TheShizuka <77496700+TheShizuka@users.noreply.github.com>
2023-10-20 15:34:35 +0200

    Update DrawerMenu.svelte

--- src/routes/DrawerMenu.svelte
+++ src/routes/DrawerMenu.svelte
@@ -1,20 +1,27 @@
 <script>
-    function toggleMenu() {
-        var menu = document.querySelector('.menu');
-        menu.classList.toggle('open');
-    }
+  let menuOpen = false;
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+    const links = document.querySelectorAll('.menu a');
+    links.forEach(link => {
+      if (window.location.pathname === link.getAttribute('href')) {
+        link.parentElement.classList.add('active');
+      }
+    });
+  });
 </script>
 
-
-    <div class="header">
-        <button class="menu-toggle" onclick="toggleMenu()">☰</button>
-        <ul class="menu">
-            <li class="active"><a href="/">Home</a></li>
-            <li><a href="/design">Design</a></li>
+<nav class="nav">
+  <button class="menu-toggle" on:click={() => menuOpen = !menuOpen}>
+    ☰
+  </button>
+  <ul class={menuOpen ? 'menu open' : 'menu'}>
+    <li class="active"><a href="/">Home</a></li>
+    <li><a href="/design">Design</a></li>
     <li><a href="/coding">Coding Projects</a></li>
     <li><a href="/3d-art">3D Content</a></li>
     <li><a href="/photography">Photography</a></li>
     <li><a href="/video">Videography</a></li>
-        </ul>
-    </div>
-
+  </ul>
+</nav>
