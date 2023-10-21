1b49e1e48f9128c7ffebec772ca1e7b489464537
TheShizuka <77496700+TheShizuka@users.noreply.github.com>
2023-10-21 15:24:38 +0200

    Update DrawerMenu.svelte

--- src/routes/DrawerMenu.svelte
+++ src/routes/DrawerMenu.svelte
@@ -1,23 +1,25 @@
 <script>
-  let menuOpen = false;
   import { onMount } from 'svelte';
+
+  let menuOpen = false;
 
   onMount(() => {
     const links = document.querySelectorAll('.menu a');
     links.forEach(link => {
-        link.addEventListener('click', () => {
-            links.forEach(link => link.parentElement.classList.remove('active'));  // Remove 'active' class from all menu items
-            link.parentElement.classList.add('active');  // Add 'active' class to the clicked menu item
-        });
+      link.addEventListener('click', () => {
+        links.forEach(link => link.parentElement.classList.remove('active'));
+        link.parentElement.classList.add('active');
+      });
     });
-});
+  });
 </script>
 
 <nav class="nav">
   <button class="menu-toggle" on:click={() => menuOpen = !menuOpen}>
     ☰
   </button>
-  <ul class={menuOpen ? 'menu open' : 'menu'}>
+  <div class={menuOpen ? 'menu-container open' : 'menu-container'}>
+    <ul class="menu">
     <li class="active"><a href="/">Home</a></li>
     <li><a href="/design">Design</a></li>
     <li><a href="/coding">Coding Projects</a></li>
@@ -25,4 +27,5 @@
     <li><a href="/photography">Photography</a></li>
     <li><a href="/video">Videography</a></li>
   </ul>
+  </div>
 </nav>
