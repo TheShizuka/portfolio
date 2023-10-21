b3915f429aa3f81bdcffeaa91b17857e4a2af01e
TheShizuka <77496700+TheShizuka@users.noreply.github.com>
2023-10-21 15:31:57 +0200

    Update DrawerMenu.svelte

--- src/routes/DrawerMenu.svelte
+++ src/routes/DrawerMenu.svelte
@@ -20,12 +20,12 @@
   </button>
   <div class={menuOpen ? 'menu-container open' : 'menu-container'}>
     <ul class="menu">
-    <li class="active"><a href="/">Home</a></li>
-    <li><a href="/design">Design</a></li>
-    <li><a href="/coding">Coding Projects</a></li>
-    <li><a href="/3d-art">3D Content</a></li>
-    <li><a href="/photography">Photography</a></li>
-    <li><a href="/video">Videography</a></li>
-  </ul>
+      <li class="active"><a href="/">Home</a></li>
+      <li><a href="/design">Design</a></li>
+      <li><a href="/coding">Coding Projects</a></li>
+      <li><a href="/3d-art">3D Content</a></li>
+      <li><a href="/photography">Photography</a></li>
+      <li><a href="/video">Videography</a></li>
+    </ul>
   </div>
 </nav>
