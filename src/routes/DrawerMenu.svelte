327ea741da94450548fdebb1fd605204be0b76f4
TheShizuka <77496700+TheShizuka@users.noreply.github.com>
2023-10-21 15:38:31 +0200

    Update DrawerMenu.svelte

--- src/routes/DrawerMenu.svelte
+++ src/routes/DrawerMenu.svelte
@@ -1,31 +1,28 @@
 <script>
-  import { onMount } from 'svelte';
-
   let menuOpen = false;
+  import { onMount } from 'svelte';
 
   onMount(() => {
     const links = document.querySelectorAll('.menu a');
     links.forEach(link => {
-      link.addEventListener('click', () => {
-        links.forEach(link => link.parentElement.classList.remove('active'));
-        link.parentElement.classList.add('active');
-      });
+        link.addEventListener('click', () => {
+            links.forEach(link => link.parentElement.classList.remove('active'));  // Remove 'active' class from all menu items
+            link.parentElement.classList.add('active');  // Add 'active' class to the clicked menu item
+        });
     });
-  });
+});
 </script>
 
 <nav class="nav">
   <button class="menu-toggle" on:click={() => menuOpen = !menuOpen}>
     ☰
   </button>
-  <div class={menuOpen ? 'menu-container open' : 'menu-container'}>
-    <ul class="menu">
-      <li class="active"><a href="/">Home</a></li>
-      <li><a href="/design">Design</a></li>
-      <li><a href="/coding">Coding Projects</a></li>
-      <li><a href="/3d-art">3D Content</a></li>
-      <li><a href="/photography">Photography</a></li>
-      <li><a href="/video">Videography</a></li>
-    </ul>
-  </div>
+  <ul class={menuOpen ? 'menu open' : 'menu'}>
+    <li class="active"><a href="/">Home</a></li>
+    <li><a href="/design">Design</a></li>
+    <li><a href="/coding">Coding Projects</a></li>
+    <li><a href="/3d-art">3D Content</a></li>
+    <li><a href="/photography">Photography</a></li>
+    <li><a href="/video">Videography</a></li>
+  </ul>
 </nav>
