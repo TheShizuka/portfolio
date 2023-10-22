e064cdc2dd98bcec8e39f21f59a8cc180951d754
TheShizuka <77496700+TheShizuka@users.noreply.github.com>
2023-10-22 10:48:32 +0200

    Update DrawerMenu.svelte

--- src/routes/DrawerMenu.svelte
+++ src/routes/DrawerMenu.svelte
@@ -1,36 +1,45 @@
 <script>
-  import { onMount } from 'svelte';
-  let menuOpen = false;
+    import { onMount } from 'svelte';
+    let menuOpen = false;
 
-  function closeMenu() {
-    menuOpen = false;
-  }
+    /* New function to close the menu */
+    function closeMenu() {
+        menuOpen = false;
+    }
 
   onMount(() => {
-    // This ensures the script runs after the component is mounted on the client side.
     const links = document.querySelectorAll('.menu a');
     links.forEach(link => {
-      link.addEventListener('click', () => {
-        closeMenu(); // we close the menu when a link is clicked
-        links.forEach(link => link.parentElement.classList.remove('active'));
-        link.parentElement.classList.add('active');
-      });
+        link.addEventListener('click', () => {
+            links.forEach(link => link.parentElement.classList.remove('active'));  // Remove 'active' class from all menu items
+            link.parentElement.classList.add('active');  // Add 'active' class to the clicked menu item
+        });
     });
-  });
+});
 </script>
+  
 
-<!-- We keep one nav and manage visibility with CSS -->
-<nav class="nav-mobile">
-  <button class="menu-toggle" on:click={() => menuOpen = !menuOpen}>☰</button>
-  <div class={menuOpen ? 'mobile-menu-open open' : 'mobile-menu-open'}>
-    <button on:click={closeMenu}>X</button>
-    <ul>
-      <li on:click={closeMenu}><a href="/" class:active={$currentPage === '/'}>Home</a></li>
+<nav class="nav">
+    <button class="menu-toggle" on:click={() => menuOpen = !menuOpen}>
+    ☰
+  </button>
+  <ul class={menuOpen ? 'menu open' : 'menu'}>
+    <li class="active"><a href="/">Home</a></li>
     <li><a href="/design">Design</a></li>
     <li><a href="/coding">Coding Projects</a></li>
     <li><a href="/3d-art">3D Content</a></li>
     <li><a href="/photography">Photography</a></li>
     <li><a href="/video">Videography</a></li>
-</ul>
-  </div>
+  </ul>
+<!-- New drawer menu for mobile -->
+    <div class={menuOpen ? 'drawer-menu open' : 'drawer-menu'}>
+        <button class="close-button" on:click={closeMenu}>×</button> <!-- Close button -->
+        <ul>
+            <!-- The same menu items, but you can style them differently or add new ones -->
+            <li on:click={closeMenu}><a href="/">Home</a></li>
+            <li on:click={closeMenu}><a href="/design">Design</a></li>
+            <li on:click={closeMenu}><a href="/coding">Coding Projects</a></li>
+            <!-- ... other menu items ... -->
+        </ul>
+    </div>
 </nav>
