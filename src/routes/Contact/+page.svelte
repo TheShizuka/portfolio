a7a4342d6dc900d5cb9c896c32bc40088a3cda04
TheShizuka <77496700+TheShizuka@users.noreply.github.com>
2023-10-24 18:33:33 +0200

    Update +page.svelte

--- src/routes/Contact/+page.svelte
+++ src/routes/Contact/+page.svelte
@@ -1,11 +1,6 @@
 <script>
-    import Contact from './Contact.svelte';
-    import DrawerMenu from './DrawerMenu.svelte';
 </script>
 
 <main>
     <h1>My Svelte Project</h1>
-    
-    <DrawerMenu />
-    <Contact/>
 </main>
