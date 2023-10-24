41a2bbae5b74b23c4e2b4cddd1bd253ddb3e5c97
TheShizuka <77496700+TheShizuka@users.noreply.github.com>
2023-10-24 13:55:53 +0200

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,7 +10,7 @@
   <title>Shizuka's Portfolio</title>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Tilt+Neon&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
 
   <div class="header">
     <DrawerMenu /> <!-- Use the DrawerMenu component -->
