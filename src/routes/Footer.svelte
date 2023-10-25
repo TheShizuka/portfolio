37d6a320d2cd38ac4ca48e797eafcc61b57d2045
TheShizuka <77496700+TheShizuka@users.noreply.github.com>
2023-10-25 17:33:16 +0200

    Update Footer.svelte

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -16,6 +16,9 @@
     .text-footer{
     margin-top: 3rem;
     }
+    .social-logo-footer{
+    margin-top: 2rem;
+    }
 
     .footer-logo img {
         width: 50px; /* Adjust based on your preference */
@@ -39,6 +42,17 @@
         margin-left: 5rem;
         margin-right: 5rem;
     }
+
+     .footer-menu a {
+        text-decoration: none;   /* Remove underline */
+        color: white;            /* Set color to white */
+        transition: 0.3s;       /* Smooth transition for hover effect */
+    }
+
+    .footer-menu a:hover {
+        text-decoration: underline;  /* Underline on hover */
+    }
+    
 
     .footer-menu ul {
         list-style: none;
@@ -96,7 +110,7 @@
     </div>
 
     <div class="social-media">
-        <ul class="text-footer">
+        <ul class="social-logo-footer">
             <li><a href="https://www.linkedin.com/in/hicham-ayat-gimenez-329a90248" target="_blank"><img src="../images/linkedin.png" alt="LinkedIn" class="social-icon"></a></li>
             <li><a href="https://www.instagram.com/shizuka.desu/" target="_blank"><img src="../images/instagram.png" alt="instagram" class="social-icon"></a></li>
             <li><a href="https://github.com/TheShizuka" target="_blank"><img src="../images/github.png" alt="github" class="social-icon"></a></li>
