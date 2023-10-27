42b0af99857a6fcbe1e1f07ffa52008119edeeb1
TheShizuka <77496700+TheShizuka@users.noreply.github.com>
2023-10-27 14:11:21 +0200

    Update ImageGrid.svelte

--- src/routes/photography/ImageGrid.svelte
+++ src/routes/photography/ImageGrid.svelte
@@ -1,18 +1,29 @@
 <script>
     export let photos;
     export let filter;
+    import photos from './images/photos.json';
 
     $: filteredPhotos = filter === 'all' ? photos : photos.filter(photo => photo.category === filter);
+    console.log(portrait1.photo.url)
 </script>
 
-<div class="image-grid">
-    {#each filteredPhotos as photo}
-        <div class="image-container">
-            <img src={photo.url} alt={photo.description} style="height: 200px; width: auto;" />
-            <!-- assuming your photo object has a url and description field -->
+<!-- Loop through each category of photos -->
+{#each photos as category}
+    <div>
+        <h2>{category.category}</h2> <!-- Display the category name -->
+        
+        <div class="image-grid">
+            <!-- Now, loop through each photo in the current category -->
+            {#each category.images as photo}
+                <div class="image-container">
+                    <!-- Now we're accessing the URL correctly through the nested structure -->
+                    <img src={photo.url} alt={photo.description} style="height: 100%; width: auto;" />
+                </div>
+            {/each}
         </div>
-    {/each}
-</div>
+    </div>
+{/each}
+
 
 <style>
     .image-grid {
