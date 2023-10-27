fad77ca27007c09ccf7a9489ff267a6d736948b4
TheShizuka <77496700+TheShizuka@users.noreply.github.com>
2023-10-27 11:50:16 +0200

    Create NavBar.svelte

---
+++ src/routes/photography/NavBar.svelte
@@ -0,0 +1,42 @@
+<script>
+    export let setFilter;
+
+    const categories = ['all', 'portrait', 'landscape', 'animal', 'japan', 'singapore'];
+
+    let selectedCategory = 'all';
+    
+    function applyFilter(category) {
+        selectedCategory = category;
+        setFilter(category);
+    }
+</script>
+
+<nav class="nav-bar">
+    <ul>
+        {#each categories as category}
+            <li
+                class:selected={selectedCategory === category}
+                on:click={() => applyFilter(category)}
+            >
+                {category}
+            </li>
+        {/each}
+    </ul>
+</nav>
+
+<style>
+    .nav-bar ul {
+        list-style-type: none;
+        /* Add your CSS for the nav bar */
+    }
+    li {
+        cursor: pointer;
+        /* More styling here */
+    }
+    li:hover {
+        /* Hover effect */
+    }
+    li:selected {
+        /* Selected item style */
+    }
+</style>
