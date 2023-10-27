148f8c4b4353cf117d09a02f3452ff9db2b806bc
TheShizuka <77496700+TheShizuka@users.noreply.github.com>
2023-10-27 11:50:32 +0200

    Create ImageGrid.svelte

---
+++ src/routes/photography/ImageGrid.svelte
@@ -0,0 +1,27 @@
+<script>
+    export let photos;
+    export let filter;
+
+    $: filteredPhotos = filter === 'all' ? photos : photos.filter(photo => photo.category === filter);
+</script>
+
+<div class="image-grid">
+    {#each filteredPhotos as photo}
+        <div class="image-container">
+            <img src={photo.url} alt={photo.description} style="height: 100%; width: auto;" />
+            <!-- assuming your photo object has a url and description field -->
+        </div>
+    {/each}
+</div>
+
+<style>
+    .image-grid {
+        display: flex;
+        flex-wrap: wrap;
+        /* Add your grid CSS here */
+    }
+    .image-container {
+        /* Style your image containers */
+    }
+    /* Add responsiveness using media queries */
+</style>
