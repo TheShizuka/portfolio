b57cdc1bd89a1dddde8634f99590bd9dc3697cef
TheShizuka <77496700+TheShizuka@users.noreply.github.com>
2023-10-27 11:59:43 +0200

    Update +page.svelte

--- src/routes/photography/+page.svelte
+++ src/routes/photography/+page.svelte
@@ -1,15 +1,9 @@
 <script>
-    import { onMount } from 'svelte';
     import NavBar from './NavBar.svelte';  // assuming you have a NavBar component
-    import ImageGrid from './ImageGrid.svelte';  // we will create this component
+    import ImageGrid from './ImageGrid.svelte';  // assuming you have an ImageGrid component
+    import photos from './photos.json'; // directly import the local JSON file
 
-    let photos = [];
     let filter = 'all';
-    
-    // This should be your actual API call or local data fetch
-    onMount(async () => {
-        photos = await fetch('path-to-your-photos-api-or-json').then(res => res.json());
-    });
 
     function setFilter(category) {
         filter = category;
@@ -24,6 +18,9 @@
 <NavBar {setFilter} />
 
 <ImageGrid {photos} {filter} />
+
+
+
 <style>
 /* Base styles */
 body {
