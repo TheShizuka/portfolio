2f75b06ee783b77df043ca9f781f450bc699da8f
TheShizuka <77496700+TheShizuka@users.noreply.github.com>
2023-10-29 10:28:18 +0100

    Update +page.svelte

--- src/routes/photography/+page.svelte
+++ src/routes/photography/+page.svelte
@@ -1,6 +1,10 @@
-
+<script>
+  import { onMount } from 'svelte';
+  import Photography from './Photography.svelte';
+</script>
 
 <div id="portfolio" style="background-image: url('../../images/background.png'); background-repeat: repeat;">
+<Photography />
 </div>
 
 
