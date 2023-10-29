980c321980a5cc1ee715e7961de900eea5cc8e0b
TheShizuka <77496700+TheShizuka@users.noreply.github.com>
2023-10-29 10:25:23 +0100

    Update +page.svelte

--- src/routes/photography/+page.svelte
+++ src/routes/photography/+page.svelte
@@ -1,39 +1,10 @@
 
 
 <div id="portfolio" style="background-image: url('../../images/background.png'); background-repeat: repeat;">
-  <div class="image-group">
-    <img src="../../images/all/1.png" alt="" class="small">
-    <img src="../../images/all/3.png"  alt="" class="large">
-    <img src="../../images/all/4.png"  alt="" class="small">
-    <img src="../../images/all/50.png"  alt="" class="large">
-  </div>
 </div>
 
 
 
 <style>
-
-#portfolio {
-    padding: 16px;
-}
-
-.image-group {
-    display: grid;
-    grid-template-columns: 2fr 1fr;
-    grid-template-rows: 1fr 1fr;
-    gap: 4px;
-    width: 1280px;
-    height: 720px;
-    overflow: hidden;
-}
-
-.large {
-    grid-row: span 2;
-    object-fit: cover;
-}
-
-.small {
-    object-fit: cover;
-}
 
 </style>
