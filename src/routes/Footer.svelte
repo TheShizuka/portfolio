ee7e8355cc46a7a6bb44d4f603875e05172e714d
TheShizuka <77496700+TheShizuka@users.noreply.github.com>
2023-10-30 13:09:39 +0100

    Update Footer.svelte

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -104,7 +104,7 @@
         <ul class="text-footer">
             <li class="active"><a href="/">Home</a></li>
             <li><a href="/photography">Photography</a></li>
-            <li><a href="/3d-art">3D Art</a></li>
+            <li><a href="/3dart">3D Art</a></li>
             <li><a href="/design">Design</a></li>
             <li><a href="/coding">Coding Projects</a></li>
             <li><a href="/video">Videography</a></li>
