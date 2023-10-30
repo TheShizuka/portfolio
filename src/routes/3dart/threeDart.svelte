4dc4ddc86ce7ed2e22b893e63211caac1013e2f3
TheShizuka <77496700+TheShizuka@users.noreply.github.com>
2023-10-30 14:31:35 +0100

    Update threeDart.svelte

--- src/routes/3dart/threeDart.svelte
+++ src/routes/3dart/threeDart.svelte
@@ -1,3 +1,18 @@
+<script>
+  import { onMount } from 'svelte';
+  let showModal = false;
+  let selectedProject = null;
+
+  function openModal(project) {
+    selectedProject = project;
+    showModal = true;
+  }
+
+  function closeModal() {
+    showModal = false;
+  }
+</script>
+
 <style>
 .description-container {
   text-align: center;
@@ -63,6 +78,26 @@
       flex-direction: column;
     }
   }
+.modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.8);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+}
+
+.modal-content {
+  background-color: #ffffff;
+  padding: 2rem;
+  max-width: 80%;
+  max-height: 80%;
+  overflow: auto;
+}
 </style>
 
 
@@ -76,19 +111,44 @@ Over time, my passion for photography has not only remained a delightful pursuit
 
 <div class="project-container">
   <!-- Project 1 -->
-  <div class="project">
+  <div class="project" on:click={() => openModal('project1')}>
     <img src="../../images/brisee.png" alt="brisée">
     <div class="text">
       <h3>Brisée : An Animated Short</h3>
-      <p>During my time as a 3D infographist student, I joined forces with three other classmates to create a short film titled "Inside the Bin." The story unfolds inside a trashcan where our main character, Elisabeth, a discarded porcelain doll, stumbles upon an intriguing new friend among the trash. Learn More...</p>
+      <p><!-- ... --></p>
     </div>
   </div>
 
   <!-- Project 2 -->
-  <div class="project reverse">
+  <div class="project reverse" on:click={() => openModal('project2')}>
     <img src="/path/to/image2.jpg" alt="Project 2 Image">
     <div class="text">
       <h3>Project Title 2</h3>
-      <p>Description of project 2...</p>
+      <p>During my time as a 3D infographist student, I joined forces with three other classmates to create a short film titled "Brisée". The story unfolds inside a trashcan where our main character, Elisabeth, a discarded porcelain doll, stumbles upon an intriguing new friend among the trash. Learn More...</p>
     </div>
   </div>
+</div>
+
+{#if showModal}
+  <div class="modal" on:click={closeModal}>
+    <div class="modal-content" on:click|stopPropagation>
+      <!-- Project 1 Details -->
+      {#if selectedProject === 'project1'}
+        <h2>Brisée : An Animated Short</h2>
+        <p><!-- Full description... --></p>
+        <!-- Embed the final short movie and additional videos/gifs -->
+        <iframe src="https://your-video-url.com" frameborder="0" allowfullscreen></iframe>
+        <!-- ... -->
+      {/if}
+
+      <!-- Project 2 Details -->
+      {#if selectedProject === 'project2'}
+        <h2>Project Title 2</h2>
+        <p><!-- Full description... --></p>
+        <!-- Embed the final short movie and additional videos/gifs -->
+        <iframe src="https://your-video-url.com" frameborder="0" allowfullscreen></iframe>
+        <!-- ... -->
+      {/if}
+    </div>
+  </div>
+{/if}
