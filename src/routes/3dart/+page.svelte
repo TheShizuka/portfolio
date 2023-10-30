42dabbd69a41cd3627fdf8c8757258c66471ea86
TheShizuka <77496700+TheShizuka@users.noreply.github.com>
2023-10-30 13:02:56 +0100

    Create +page.svelte

---
+++ src/routes/3dart/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { onMount } from 'svelte';
+  import 3dart from './3dart.svelte';
+</script>
+
+<div id="portfolio" style="background-image: url('../../images/background.png'); background-repeat: repeat;">
+<3dart />
+</div>
+
+
+
+<style>
+
+</style>
