cf28775e4c0186d4262b2b718858282d633f16bd
TheShizuka <77496700+TheShizuka@users.noreply.github.com>
2023-10-30 14:07:17 +0100

    Update threeDart.svelte

--- src/routes/3dart/threeDart.svelte
+++ src/routes/3dart/threeDart.svelte
@@ -26,6 +26,43 @@
     font-size: 0.7rem; /* Adjust font size as needed */
   }
 }
+
+.project-container {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+  }
+
+  .project {
+    display: flex;
+    flex-direction: row;
+    gap: 1rem;
+    align-items: center;
+  }
+
+  .project.reverse {
+    flex-direction: row-reverse;
+  }
+
+  .project img {
+    max-width: 300px;
+    width: 100%;
+  }
+
+  .project .text {
+    max-width: 400px;
+  }
+
+  @media (max-width: 768px) {
+    .project {
+      flex-direction: column;
+      text-align: center;
+    }
+
+    .project.reverse {
+      flex-direction: column;
+    }
+  }
 </style>
 
 
@@ -35,3 +72,23 @@
   <h2>My Photography Journey</h2>
   <p>
 Over time, my passion for photography has not only remained a delightful pursuit of curiosity and joy, but has also seen a substantial honing of skills. Each captured moment reflects a blend of enthusiasm and a step forward in my photographic journey. As I reminisce through these frames, the eagerness to further refine my craft only intensifies. I am excited for the continuous journey of discovery and improvement that lies ahead, and I invite you to stroll through my evolving gallery of memories below.</div>
+
+
+<div class="project-container">
+  <!-- Project 1 -->
+  <div class="project">
+    <img src="/path/to/image1.jpg" alt="Project 1 Image">
+    <div class="text">
+      <h3>Project Title 1</h3>
+      <p>Description of project 1...</p>
+    </div>
+  </div>
+
+  <!-- Project 2 -->
+  <div class="project reverse">
+    <img src="/path/to/image2.jpg" alt="Project 2 Image">
+    <div class="text">
+      <h3>Project Title 2</h3>
+      <p>Description of project 2...</p>
+    </div>
+  </div>
