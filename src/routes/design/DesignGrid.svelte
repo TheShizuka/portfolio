8574c4cdb0c4bf0737daab7e3fa543173ca6fcf6
TheShizuka <77496700+TheShizuka@users.noreply.github.com>
2023-10-31 22:44:23 +0100

    Create DesignGrid.svelte

---
+++ src/routes/design/DesignGrid.svelte
@@ -0,0 +1,33 @@
+<script>
+    import DesignItem from './DesignItem.svelte';
+
+    export let designs = [];
+
+    let selectedIndex = -1;
+
+    const selectDesign = (index) => {
+        selectedIndex = index;
+    };
+
+    const nextDesign = () => {
+        if (selectedIndex < designs.length - 1) {
+            selectedIndex++;
+        }
+    };
+
+    const prevDesign = () => {
+        if (selectedIndex > 0) {
+            selectedIndex--;
+        }
+    };
+</script>
+
+{#each designs as design, index}
+<DesignItem 
+    design={design} 
+    isSelected={selectedIndex === index}
+    selectDesign={() => selectDesign(index)}
+    nextDesign={nextDesign}
+    prevDesign={prevDesign}
+/>
+{/each}
