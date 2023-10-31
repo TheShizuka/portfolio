425074547c179db8fb3bdb6e6024febc61fa572b
TheShizuka <77496700+TheShizuka@users.noreply.github.com>
2023-10-31 22:55:13 +0100

    Update DesignItem.svelte

--- src/routes/design/DesignItem.svelte
+++ src/routes/design/DesignItem.svelte
@@ -12,9 +12,11 @@
 
 
 <style>
-img {
-    width: 100%;
+.logos {
+    max-width: 300px;
     cursor: pointer;
+    display: block;
+    margin: 0 auto; /* Center the image in its container */
 }
 
 .overlay {
@@ -28,11 +30,12 @@ img {
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    padding: 2rem;
 }
 
 .overlay img {
-    max-width: 70%;
-    max-height: 70%;
+    max-width: 600px;
+    max-height: 600px;
     margin-bottom: 1rem;
 }
 
@@ -42,11 +45,50 @@ img {
     margin: 0.5rem;
 }
 
+/* Button Styles */
+button {
+    background: rgba(255, 255, 255, 0.3);
+    border: none;
+    color: white;
+    padding: 0.5rem 1rem;
+    margin: 0.5rem;
+    cursor: pointer;
+}
+
+/* Media Queries for Responsiveness */
+@media screen and (max-width: 600px) {
+    .overlay img {
+        max-width: 80%;
+        max-height: 80%;
+    }
+}
+
+/* Button Positions */
+button:nth-child(4) {  /* Left Arrow */
+    position: absolute;
+    left: 5%;
+    top: 50%;
+    transform: translateY(-50%);
+}
+
+button:nth-child(5) {  /* Right Arrow */
+    position: absolute;
+    right: 5%;
+    top: 50%;
+    transform: translateY(-50%);
+}
+
+button:nth-child(6) {  /* Close Button */
+    position: absolute;
+    right: 5%;
+    top: 5%;
+}
+
 </style>
 
 
 
-<img src={design.imgSrc} alt={design.title} on:click={selectDesign} />
+<img class="logos" src={design.imgSrc} alt={design.title} on:click={selectDesign} />
 {#if isSelected}
 <div class="overlay">
     <img src={design.imgSrc} alt={design.title} />
