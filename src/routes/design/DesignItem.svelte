6e9bf3e9f128373633d2f3860e94e13b90ea63ef
TheShizuka <77496700+TheShizuka@users.noreply.github.com>
2023-10-31 22:43:53 +0100

    Create DesignItem.svelte

---
+++ src/routes/design/DesignItem.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let design = {
+        title: "",
+        description: "",
+        imgSrc: ""
+    };
+    export let isSelected = false;
+    export let selectDesign = () => {};
+    export let nextDesign = () => {};
+    export let prevDesign = () => {};
+</script>
+
+<img src={design.imgSrc} alt={design.title} on:click={selectDesign} />
+{#if isSelected}
+<div class="overlay">
+    <img src={design.imgSrc} alt={design.title} />
+    <h2>{design.title}</h2>
+    <p>{design.description}</p>
+    <button on:click={prevDesign}>&lt;</button>
+    <button on:click={nextDesign}>&gt;</button>
+    <button on:click={() => isSelected = false}>X</button>
+</div>
+{/if}
