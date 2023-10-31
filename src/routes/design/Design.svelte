2427b432fe8a87ba96a48b1d4dec9afcfd0e8077
TheShizuka <77496700+TheShizuka@users.noreply.github.com>
2023-10-31 22:46:47 +0100

    Update Design.svelte

--- src/routes/design/Design.svelte
+++ src/routes/design/Design.svelte
@@ -36,3 +36,16 @@
   <h2>My Design Odyssey</h2>
   <p>
 As I embarked on my journey into the world of design, I quickly realized its complexities and the nuances that make a design stand out. While I consider myself to be at the early stages of this adventure, each logo or design piece I've created reflects my earnest efforts to learn and grow. These projects, primarily exercises and practice runs, represent my initial steps into this vast field. I'm eager to continue honing my skills, learning from every project, and gradually building my design repertoire. Here's a glimpse of my starting ventures in design.</p></div>
+
+
+<script>
+    import DesignGrid from './DesignGrid.svelte';
+
+    let designs = [
+        {title: "Logo 1", description: "Description 1", imgSrc: "../../images/logos/1.png"},
+        {title: "Logo 2", description: "Description 2", imgSrc: "../../images/logos/2.png"},
+        // ... other designs
+    ];
+</script>
+
+<DesignGrid designs={designs} />
