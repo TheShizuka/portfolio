a34411e774f49ca2cb094dc4350ab7c946efda0e
TheShizuka <77496700+TheShizuka@users.noreply.github.com>
2023-10-31 22:45:46 +0100

    Update DesignItem.svelte

--- src/routes/design/DesignItem.svelte
+++ src/routes/design/DesignItem.svelte
@@ -10,6 +10,42 @@
     export let prevDesign = () => {};
 </script>
 
+
+<style>
+img {
+    width: 100%;
+    cursor: pointer;
+}
+
+.overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background: rgba(0, 0, 0, 0.9);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+.overlay img {
+    max-width: 70%;
+    max-height: 70%;
+    margin-bottom: 1rem;
+}
+
+.overlay h2, .overlay p {
+    color: white;
+    text-align: center;
+    margin: 0.5rem;
+}
+
+</style>
+
+
+
 <img src={design.imgSrc} alt={design.title} on:click={selectDesign} />
 {#if isSelected}
 <div class="overlay">
