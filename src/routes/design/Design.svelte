b85b90cc0484feeaadf6cbe078ca4a5acb1f262b
TheShizuka <77496700+TheShizuka@users.noreply.github.com>
2023-10-31 22:13:13 +0100

    Create Design.svelte

---
+++ src/routes/design/Design.svelte
@@ -0,0 +1,38 @@
+<style>
+.description-container {
+  text-align: center;
+  padding: 3rem 1rem; /* Adjust padding as needed */
+  margin-bottom: 2rem; /* Adds some space between the description and the grid */
+}
+
+.description-container h2 {
+  font-size: 1.8rem; /* Adjust font size as needed */
+  margin-bottom: 1rem; /* Adds some space between the title and the description */
+  color: #ffffff;
+}
+
+.description-container p {
+  max-width: 800px;
+  margin: 0 auto; /* Centers the paragraph if it's narrower than its container */
+  font-size: 1rem; /* Adjust font size as needed */
+  color: #ffffff;
+}
+@media (max-width: 768px) {
+  .description-container h2 {
+    font-size: 1.2rem; /* Adjust font size as needed */
+  }
+
+  .description-container p {
+    font-size: 0.7rem; /* Adjust font size as needed */
+  }
+}
+
+</style>
+
+
+
+
+<div class="description-container">
+  <h2>My Photography Journey</h2>
+  <p>
+Over time, my passion for photography has not only remained a delightful pursuit of curiosity and joy, but has also seen a substantial honing of skills. Each captured moment reflects a blend of enthusiasm and a step forward in my photographic journey. As I reminisce through these frames, the eagerness to further refine my craft only intensifies. I am excited for the continuous journey of discovery and improvement that lies ahead, and I invite you to stroll through my evolving gallery of memories below.</p></div>
