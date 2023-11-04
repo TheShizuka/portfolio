887ad832e7a5bf9cd7baeaefbeb449bf14590f8e
TheShizuka <77496700+TheShizuka@users.noreply.github.com>
2023-11-04 10:59:10 +0100

    Create +page.svelte

---
+++ src/routes/Coding/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { onMount } from 'svelte';
+  import Coding from './Coding.svelte';
+</script>
+
+<div id="portfolio" style="background-image: url('../../images/background.png'); background-repeat: repeat;">
+  <Coding />
+</div>
+
+<style>
+  /* your styles */
+</style>
