26093f7fa88c9bc35e2b764f0ff8b36458e835b4
TheShizuka <77496700+TheShizuka@users.noreply.github.com>
2023-11-08 11:38:46 +0100

    Create analytics.svelte

---
+++ src/routes/analytics.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { page } from '$app/stores'
+
+  $: {
+    if (typeof gtag !== 'undefined') {
+      gtag('config', 'G-2RLJZ35B3Y', {
+        page_title: document.title,
+        page_path: $page.url.pathname,
+      })
+    }
+  }
+</script>
+
+<svelte:head>
+  <script
+    async
+    src="https://www.googletagmanager.com/gtag/js?id=G-2RLJZ35B3Y">
+  </script>
+  <script>
+    window.dataLayer = window.dataLayer || []
+
+    function gtag() {
+      dataLayer.push(arguments)
+    }
+
+    gtag('js', new Date())
+    gtag('config', 'G-2RLJZ35B3Y')
+  </script>
+</svelte:head>
